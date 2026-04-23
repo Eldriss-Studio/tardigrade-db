e2a3026546a33b36ce18438891023ad113c5759a
Flagrare <Flagrare@users.noreply.github.com>
2026-04-23 19:55:12 +0000

    deploy: f496705a3d88a82371e47ad278b535cbc5ff8df7

--- tdb_engine/engine/sidebar-items.js
+++ tdb_engine/engine/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["DEFAULT_SLB_CAPACITY","DEFAULT_VAMANA_MAX_DEGREE","DEFAULT_VAMANA_THRESHOLD"],"struct":["CellGovernance","Engine","ReadResult","VamanaAdapter"]};
+window.SIDEBAR_ITEMS = {"constant":["DEFAULT_SLB_CAPACITY","DEFAULT_VAMANA_MAX_DEGREE","DEFAULT_VAMANA_THRESHOLD"],"struct":["CellGovernance","Engine","ReadResult","VamanaAdapter","WriteRequest"]};
