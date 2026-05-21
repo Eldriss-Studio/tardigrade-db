601deaaad79c171a546c00b5adca4e590b490d7a
Flagrare <Flagrare@users.noreply.github.com>
2026-05-21 02:35:48 +0000

    deploy: 0bb42be647462b53ef9c5f1fe094b90a6812f9e7

--- trait.impl/tdb_retrieval/cell_source/trait.CellSource.js
+++ trait.impl/tdb_retrieval/cell_source/trait.CellSource.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["tdb_engine",[["impl <a class=\"trait\" href=\"tdb_retrieval/cell_source/trait.CellSource.html\" title=\"trait tdb_retrieval::cell_source::CellSource\">CellSource</a> for <a class=\"struct\" href=\"tdb_engine/cell_source_pool/struct.BlockPoolCellSource.html\" title=\"struct tdb_engine::cell_source_pool::BlockPoolCellSource\">BlockPoolCellSource</a>&lt;'_&gt;",0]]]]);
+    const implementors = Object.fromEntries([["tdb_engine",[["impl CellSource for <a class=\"struct\" href=\"tdb_engine/cell_source_pool/struct.BlockPoolCellSource.html\" title=\"struct tdb_engine::cell_source_pool::BlockPoolCellSource\">BlockPoolCellSource</a>&lt;'_&gt;",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[367]}
+//{"start":59,"fragment_lengths":[232]}
